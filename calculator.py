dd7693670f892dfd871cf52d2a5afce8d2a51540
ammar-faifi <a@ammarf.com>
2022-07-30 11:28:35 +0300

    init commint

---
+++ calculator.py
@@ -0,0 +1,26 @@
+"""
+This module provides an example of calculator moethods to be used
+later with `uittest` module for testing.
+"""
+
+def add(num1, num2):
+    """Adds `num1` and `num2`"""
+
+    # raise Exception("Erorr in `add`")
+    # return None
+    return num1 + num2
+
+def sub(num1, num2):
+    """Subtracts `num2` form `num1`"""
+
+    return num1 - num2
+
+def mul(num1, num2):
+    """Muliplies `num1` and `num2`"""
+
+    return num1 * num2
+
+def div(num1, num2):
+    """Divides `num1` to `num2`"""
+
+    return num1 / num2
---
+++ calculator_tests.py
@@ -0,0 +1,57 @@
+"""
+This module provides an example of calculator testing.
+
+Notes:
+    1- Each class naming must be in form `*TestCase`
+    2- Each method name must be in form `test_*`
+        otherwise it wont be run.
+    3- If a test fails it raises an `AssertionError`
+"""
+
+from unittest import TestCase
+from calculator import add, sub, mul, div
+
+class CalculatorTestCase(TestCase):
+    """Test case class for `calculator`"""
+
+    def setUp(self) -> None:
+        self.a = 10
+        self.b = 2
+
+    def tearDown(self) -> None:
+        del self.a
+        del self.b
+
+
+    # All tests
+
+    def test_add(self):
+        """Test `add` method"""
+
+        # self.assertIsNotNone(add(self.a, self.b), "add must not return None")
+        # self.assertEqual(add(self.a, self.b), 12)
+
+        assert add(self.a, self.b) is not None #, "add must not return None"
+        assert add(self.a, self.b) == 12
+
+    def test_sub(self) -> None:
+        """Test `sub` method"""
+
+        assert sub(self.a, self.b) == 8
+
+    def test_mul(self) -> None:
+        """Test `mul` method"""
+
+        assert mul(self.a, self.b) == 20
+
+    def test_div(self) -> None:
+        """Test `div` method"""
+
+        assert div(self.a, self.b) == 5
+
+        div(1, 0)
+
+    def general_method(self):
+        """Not-a-test method"""
+
+        print("This method is not a test")
